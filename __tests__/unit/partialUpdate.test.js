6e5d4271fc0c2b68a925ace1f25d86ba5d861d94
Chris Martin <chris@sensedev.com>
2019-09-11 10:17:34 -0700

    completed first jest test. it's not so scary

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,25 @@
-describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+const sqlForPartialUpdate = require('../../helpers/partialUpdate');
 
+process.env.NODE_ENV = 'test';
+
+describe("Testing SQL partial update function", function () {
+
+  test('function returns expected value', function () {
+    let testItems = {
+      item1: 'orange',
+      item2: 'blue'
+    }
+    let table = 'testTable';
+    let key = 'testKey';
+    let id = 10
+
+    let result = sqlForPartialUpdate(table, testItems, key, id);
+    expect(result).toEqual({
+      query: `UPDATE ${table} SET item1=$1, item2=$2 WHERE testKey=$3 RETURNING *`,
+      values: ['orange', 'blue', 10]
+    });
   });
 });
+
+
